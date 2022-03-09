3b0e6b64a82d1210fdd85f0942af41204d819d55
smulaMat <smula.mat@gmail.com>
2022-03-09 21:50:44 +0100

    ChanchesWedne

---
+++ myBioRepo.js
@@ -0,0 +1,44 @@
+(function(){
+
+    function createButton(){
+    
+        var button = document.createElement("button");
+    
+        button.classList.add("backToTop", "hidden");
+        document.body.appendChild(button);
+        button.textContent = "Back to Top"
+    
+        return button;
+    
+    }
+    
+    var button = createButton();
+    
+    function animateScroll() {
+       
+        if(document.documentElement.scrollTop > 0){
+         window.scrollBy(0, document.documentElement.scrollTop*(-0.1))
+         setTimeout(animateScroll, 20)    
+     }
+     
+     }
+    
+    button.addEventListener("click", function (e) {
+        
+        e.stopPropagation();
+    
+        animateScroll();
+    
+        }, false)
+    
+    window.addEventListener("scroll", function(e) {
+        
+        if(document.documentElement.scrollTop >= 100){
+            button.classList.remove("hidden");
+        } else {
+            button.classList.add("hidden")
+        }
+    
+    },false)
+    
+    })(); 
